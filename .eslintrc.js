d2ad0eb9ed48edfe05991f99d024d50d19c4ac0f
Avneesh Kumar <avneesh.kumar10@gmail.com>
2024-02-28 14:33:58 +0530

    initial update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
+  overrides: [
+    {
+      files: ['*.ts', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        "react-native/no-inline-styles": 0,
+        "no-inline-styles": "off",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["off"]
+      },
+    },
+  ],
   extends: '@react-native',
 };
